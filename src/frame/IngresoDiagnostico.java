edada686e632d150e328f0947c2048bb4af2ca2e
Mauro Trotta <mauroatrotta@gmail.com>
2017-06-16 08:35:46 -0300

    Validaciones a campos
    Se agregan validaciones para que los campos no esten vacios en ingreso
    de diagnostico

--- src/frame/IngresoDiagnostico.java
+++ src/frame/IngresoDiagnostico.java
@@ -21,9 +21,14 @@ import java.awt.event.ActionListener;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
+import javax.swing.border.TitledBorder;
+
+import com.toedter.calendar.JDateChooser;
+
 
 public class IngresoDiagnostico extends JFrame {
 
@@ -32,6 +37,8 @@ public class IngresoDiagnostico extends JFrame {
 	private final JComboBox comboBoxMedico,comboBoxPaciente ;
 	private final JLabel lblNombreMedico;
 	private int codigoMedico ;
+	private JDateChooser dateChooserFecha;
+	private JLabel lblResult;
 	
 	private final DBControlador conector = new DBControlador();
 	/**
@@ -58,52 +65,56 @@ public class IngresoDiagnostico extends JFrame {
 		
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setTitle("CENTRO MEDICO LOS LAURELES");
-		setBounds(100, 100, 575, 294);
+		setTitle("SITUACION PACIENTE - CENTRO MEDICO LOS LAURELES");
+		setBounds(100, 100, 575, 364);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
+		
 		JLabel lblCdigoDelPaciente = new JLabel("C\u00F3digo del paciente:");
 		lblCdigoDelPaciente.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblCdigoDelPaciente.setBounds(10, 53, 121, 14);
+		lblCdigoDelPaciente.setBounds(10, 106, 121, 14);
 		panel.add(lblCdigoDelPaciente);
 		
 		JLabel lblCdigoDelMdico = new JLabel("C\u00F3digo del m\u00E9dico:");
 		lblCdigoDelMdico.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblCdigoDelMdico.setBounds(10, 78, 103, 14);
+		lblCdigoDelMdico.setBounds(10, 136, 103, 14);
 		panel.add(lblCdigoDelMdico);
 		
 		JLabel lblDatosDelPaciente = new JLabel("Ingrese el diagn\u00F3stico");
 		lblDatosDelPaciente.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDatosDelPaciente.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblDatosDelPaciente.setFont(new Font("Arial", Font.BOLD, 16));
 		lblDatosDelPaciente.setBounds(295, 11, 244, 20);
 		panel.add(lblDatosDelPaciente);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Diagnóstico", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(10, 164, 529, 106);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
 		textAreaDiagnostico = new JTextArea();
+		textAreaDiagnostico.setBounds(20, 16, 499, 83);
+		panel_1.add(textAreaDiagnostico);
+		textAreaDiagnostico.setLineWrap(true);
 		textAreaDiagnostico.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		textAreaDiagnostico.setBounds(141, 106, 398, 83);
-		panel.add(textAreaDiagnostico);
 		
-		final JLabel lblResultado = new JLabel("");
-		lblResultado.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblResultado.setBounds(10, 216, 161, 14);
-		panel.add(lblResultado);
-		
-		comboBoxMedico = new JComboBox();
-		
+			
+		comboBoxMedico = new JComboBox();		
 		comboBoxMedico.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		comboBoxMedico.setBounds(141, 75, 398, 20);
+		comboBoxMedico.setBounds(141, 133, 398, 20);
 		panel.add(comboBoxMedico);
 		
 		comboBoxPaciente = new JComboBox();
 		comboBoxPaciente.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		comboBoxPaciente.setBounds(141, 50, 398, 20);
+		comboBoxPaciente.setBounds(141, 103, 398, 20);
 		panel.add(comboBoxPaciente);
 		
 		this.llenarPacientes();
@@ -118,38 +129,64 @@ public class IngresoDiagnostico extends JFrame {
 			}
 		});
 		btnCancelar.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnCancelar.setBounds(450, 216, 89, 23);
+		btnCancelar.setBounds(449, 281, 89, 23);
 		panel.add(btnCancelar);
 		
 		JButton btnGuardar = new JButton("Guardar");
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(conector.insertarDiagnostico(comboBoxMedico.getSelectedItem(), comboBoxPaciente.getSelectedItem(), textAreaDiagnostico.getText())){
-					lblResultado.setText("Grabado");
-					lblResultado.setForeground(Color.GREEN);
-					refrescarPacientes();
-					refrescarMedicos();
-					textAreaDiagnostico.setText("");
-					
+				SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+				
+				if(dateChooserFecha.getDate() == null || dateChooserFecha.getDate().equals("")){
+					lblResult.setText("El campo fecha no puede estar vacío");
+					lblResult.setForeground(Color.RED);
 				} else {
-					lblResultado.setText("Error al grabar");
-					lblResultado.setForeground(Color.RED);
+					String formatted = format1.format(dateChooserFecha.getDate());
+					
+					if(!validarCampos(comboBoxMedico.getSelectedItem(), comboBoxPaciente.getSelectedItem(),textAreaDiagnostico.getText())){
+						//lblResult.setText("Complete todos los campos");
+						//lblResult.setForeground(Color.RED);
+					} else {
+						if(conector.insertarDiagnostico(comboBoxMedico.getSelectedItem(), comboBoxPaciente.getSelectedItem(), textAreaDiagnostico.getText(),formatted)){
+							lblResult.setText("Grabado");
+							lblResult.setForeground(Color.GREEN);
+							refrescarPacientes();
+							refrescarMedicos();
+							textAreaDiagnostico.setText("");
+							
+						} else {
+							lblResult.setText("Error al grabar");
+							lblResult.setForeground(Color.RED);
+						}
+					}
 				}
+				
+				
+				
 			}
 		});
 		btnGuardar.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnGuardar.setBounds(337, 216, 103, 23);
+		btnGuardar.setBounds(336, 281, 103, 23);
 		panel.add(btnGuardar);
-		
-		JLabel lblDiagnstico = new JLabel("Diagn\u00F3stico:");
-		lblDiagnstico.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblDiagnstico.setBounds(10, 108, 76, 14);
-		panel.add(lblDiagnstico);
 		
 		lblNombreMedico = new JLabel("");
 		lblNombreMedico.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		lblNombreMedico.setBounds(285, 109, 244, 14);
 		panel.add(lblNombreMedico);
+		
+		JLabel lblFecha = new JLabel("Fecha:");
+		lblFecha.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblFecha.setBounds(10, 72, 121, 14);
+		panel.add(lblFecha);
+		
+		dateChooserFecha = new JDateChooser();
+		dateChooserFecha.setBounds(141, 72, 138, 20);
+		panel.add(dateChooserFecha);
+		
+		lblResult = new JLabel("");
+		lblResult.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblResult.setBounds(10, 285, 253, 14);
+		panel.add(lblResult);
 
 	}
 	
@@ -189,5 +226,28 @@ public class IngresoDiagnostico extends JFrame {
 	private void refrescarMedicos(){
 		this.comboBoxMedico.removeAllItems();	
 		llenarMedicos();	
+	}
+	
+	private boolean validarCampos(Object m, Object p, String diagnostico){
+		ComboBoxItem medico = (ComboBoxItem) p;
+		ComboBoxItem paciente = (ComboBoxItem) m;
+		
+		if(medico.getValue().equals(null) || medico.getValue().equals("") ){
+			lblResult.setText("Campo médico no puede estar vacío");
+			lblResult.setForeground(Color.RED);
+			return false;
+		}
+		if(paciente.getValue().equals(null) || paciente.getValue().equals("") ){
+			lblResult.setText("Campo paciente no puede estar vacío");
+			lblResult.setForeground(Color.RED);
+			return false;
+		}
+		if(diagnostico.equals(null)||diagnostico.equals("")){
+			lblResult.setText("Campo diagnóstico no puede estar vacío");
+			lblResult.setForeground(Color.RED);
+			return false;
+		}
+		
+		return true;
 	}
 }
